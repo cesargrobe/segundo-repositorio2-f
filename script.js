cab8bd75461cf7a2eb1f2550a4dd30f6c313660d
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2025-06-26 21:27:21 -0300

    Aula 2 - 26/06

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave() {
-    alert("Fui clicado!");
+    //alert("Fui clicado!");
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+
+    campoResultado.textContent = texto;
 }
