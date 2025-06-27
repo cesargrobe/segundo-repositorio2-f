1632ce70051e7fb9d53551cb69e4cb255ea3320b
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2025-06-26 22:05:34 -0300

    Aula 3 - 26/06

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ function mostraPalavraChave() {
     //alert("Fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ")
+    const palavras = processaTexto(texto);
 
     campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+    //let palavras = texto.split(/\s+/);  "retirar espaços"
+    //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
+    let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    let frequencias = [];
+
+    for ( let i in palavras) {
+        frequencias[i] = 0;
+        for ( let j in palavras) {
+            if(palavras[i] == palavras[j]) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
+    return palavras;
 }
